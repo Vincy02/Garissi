19b14136bf442c3dad2c711f45f662c43e6aa141
Vincenzo Monopoli <monovinci@gmail.com>
2024-05-17 20:25:23 +0200

    First setUp

---
+++ Scripts/fabrizio.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@export var speed: int = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = Vector2(100, 500)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
+	velocity = direction * speed
+	move_and_slide()
