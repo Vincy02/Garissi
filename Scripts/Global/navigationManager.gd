5d68f38fe88e0fda1915f7519f720db2e6f47d53
roccomennea <r.mennea7@studenti.uniba.it>
2024-06-12 20:37:59 +0200

    Added barInside scene and door

--- Scripts/Global/navigationManager.gd
+++ Scripts/Global/navigationManager.gd
@@ -3,6 +3,7 @@ extends Node
 const scene_square = preload("res://Scenes/square.tscn")
 const scene_park = preload("res://Scenes/park.tscn")
 const scene_cityCentre = preload("res://Scenes/cityCentre.tscn")
+const scene_barInside = preload("res://Scenes/barInside.tscn")
 signal on_trigger_player_spawn
 
 var spawn_door_tag 
@@ -17,6 +18,8 @@ func go_to_level(level_tag, destination_tag):
 			scene_to_load = scene_park
 		"cityCentre":
 			scene_to_load = scene_cityCentre		
+		"barInside":
+			scene_to_load = scene_barInside	
 	
 	if scene_to_load != null:
 		TransitionScreen.transition()
