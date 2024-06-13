909b83994dee5b4db5acebfe73d4f41ec55d3193
roccomennea <r.mennea7@studenti.uniba.it>
2024-06-13 18:26:53 +0200

    Created InputSettings scene and script and implemented a small menu to visualize it into square scene

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,10 +1,24 @@
 extends Node2D
 
+@onready var pause_menu = $CanvasLayer/InputSettings
+
+var game_paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if NavigationManager.spawn_door_tag != null:
 		_on_level_spawn(NavigationManager.spawn_door_tag)
+		
+func _process(delta):
+	if Input.is_action_just_pressed("Pause"):
+		game_paused = !game_paused
+		if game_paused:
+			Engine.time_scale = 0
+			pause_menu.visible = true
+		else:
+			Engine.time_scale = 1
+			pause_menu.visible = false
+		get_tree().root.get_viewport().set_input_as_handled()
 	
 func _on_level_spawn(destination_tag: String):
 	var door_path = "Doors/Door_" + destination_tag
