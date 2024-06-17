7008011e08f1abf332786ffd2c5aae6fd5e64918
roccomennea <r.mennea7@studenti.uniba.it>
2024-06-17 19:13:50 +0200

    Added upZone door and scene

--- Scripts/Global/navigationManager.gd
+++ Scripts/Global/navigationManager.gd
@@ -5,6 +5,7 @@ const scene_park = preload("res://Scenes/park.tscn")
 const scene_cityCentre = preload("res://Scenes/cityCentre.tscn")
 const scene_barInside = preload("res://Scenes/barInside.tscn")
 const scene_newsStandInside = preload("res://Scenes/newsStandInside.tscn")
+const scene_upZone = preload("res://Scenes/upZone.tscn")
 signal on_trigger_player_spawn
 
 var spawn_door_tag 
@@ -23,6 +24,8 @@ func go_to_level(level_tag, destination_tag):
 			scene_to_load = scene_barInside	
 		"newsStandInside":
 			scene_to_load = scene_newsStandInside	
+		"upZone":
+			scene_to_load = scene_upZone	
 	
 	if scene_to_load != null:
 		TransitionScreen.transition()
