9265a62237db23ef47db2c8e0636015cab98930d
Vincenzo Monopoli <monovinci@gmail.com>
2024-06-25 17:57:00 +0200

    Fixed load first scene bug pause menu

--- Scripts/Global/scenesManager.gd
+++ Scripts/Global/scenesManager.gd
@@ -1,8 +1,8 @@
 extends Node
 
-@onready var square = preload("res://Scenes/square.tscn") as PackedScene
+@onready var park = "res://Scenes/park.tscn"
 
 func _start_game() -> void:
-	get_tree().change_scene_to_packed(square)
-	Player.set_player_position(Vector2(1316, 740), "left")
+	get_tree().change_scene_to_file(park)
+	Player.set_player_position(Vector2(1316, 740), "right")
 	
